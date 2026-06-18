f1127eebe7bb6bcf8acb54e64be3ac5ac7382e30
Shubham Shubham Kaushiki <shubham@Kaushus-MacBook-Air.local>
2026-06-18 13:53:33 +0530

    Updates

--- _next/static/RQOXNARSbYBWy9y1entlI/_buildManifest.js
+++ _next/static/RQOXNARSbYBWy9y1entlI/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/TIP//_next/:path+",
+        "destination": "/TIP/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
